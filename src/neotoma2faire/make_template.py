114059eff91700e4db427cd25685f1316cf29cc7
Socorro DominguezVidana <seiryu88@icloud.com>
2026-03-13 18:48:43 -0700

    BU354 Fixing from different places, Most important, using R package for retrieving data and APIs instead of connecting to db for security and so anyone can use with any ds

--- src/neotoma2faire/make_template.py
+++ src/neotoma2faire/make_template.py
@@ -1,7 +1,9 @@
 from openpyxl import load_workbook
 #from .modify_README import modify_README
 #from .add_project import add_project
+from .get_data import get_data
 from .add_samples import add_samples
+from .add_taxa import add_taxa
 
 def make_template(args):
     wb = load_workbook(filename = args.template)
@@ -9,7 +11,14 @@ def make_template(args):
     #modify_README(newsheet)
     #add_project(newsheet, args.dataset)
     #add_samples(newsheet, args.dataset)
-    df = add_samples(wb, args.dataset)
+    data = get_data(args.dataset)
+    smp = add_samples(wb, data)
+    tx_ids = data['taxonid'].unique().tolist()
+    tx = add_taxa(wb, tx_ids)
     #wb.save(args.output)
-    df.to_csv('file.csv', index=False)
+
+    # Okoboji Style
+    df = tx.merge(smp, left_on='most_specific_id', right_on='taxonid',  how='left')
+    df = df.drop(columns=['taxonid', 'most_specific_id', 'most_specific_name'])
+    df.to_csv('trial_.csv', index=False)
     return df
